5f858e1ea6ef9c8b665ea7727c4d3652ec1b9dcb
hangeer <hangeerh@sina.com>
2025-12-29 03:05:28 +0800

    Get an element from tensor

--- src/include/tensor.hpp
+++ src/include/tensor.hpp
@@ -6,21 +6,52 @@ namespace transformer {
 template <typename T> class Tensor {
 private:
   std::vector<T> m_data;
-  std::vector<int> m_shape;
+  std::vector<size_t> m_shape;
+  std::vector<size_t> m_strides;
+
+  void set_strides() {
+    size_t last = m_shape.size() - 1;
+    size_t stride = 1;
+
+    for (size_t i = last; i > 0; --i) {
+      stride *= m_shape.at(i);
+      m_strides.at(i - 1) = stride;
+    }
+
+    m_strides.back() = 1;
+  }
+
+  size_t calculate_index(const std::vector<size_t> &indices) {
+    size_t index = 1;
+    size_t count = indices.size();
+
+    for (size_t i = 0; i < count; i++) {
+      index += indices.at(i) * m_strides.at(i);
+    }
+
+    return index;
+  }
 
 public:
-  Tensor(const std::vector<int> &shape) {
+  Tensor(const std::vector<size_t> &shape) {
     m_shape = shape;
+    m_strides = std::vector<size_t>(m_shape.size());
+
+    set_strides();
+
     size_t total_elements = 1;
     for (size_t i : m_shape) {
       total_elements *= i;
     }
-    m_data.reserve(total_elements);
+
+    m_data = std::vector<T>(total_elements);
   }
 
-  Tensor();
+  std::vector<size_t> shape() { return m_shape; }
 
-  std::vector<int> shape() { return m_shape; }
+  T get(const std::vector<size_t> &indices) {
+    return m_data.at(calculate_index(indices));
+  }
 };
 
 } // namespace transformer
--- tests/test_tensor_shape.cpp
+++ tests/test_tensor_shape.cpp
@@ -2,7 +2,7 @@
 #include <cassert>
 
 int main() {
-  std::vector<int> shape = {2, 2};
+  std::vector<size_t> shape = {2, 2};
   transformer::Tensor<int> tensor(shape);
   assert(tensor.shape() == shape);
 }
