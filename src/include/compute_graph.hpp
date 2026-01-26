e13089429601cc1d6ced4dfd76c3295a215edc84
hangeer <hangeerh@sina.com>
2026-01-26 16:13:44 -0600

    compute graph progress

--- src/include/compute_graph.hpp
+++ src/include/compute_graph.hpp
@@ -1,40 +1,27 @@
 #pragma once
 #include "tensor.hpp"
+#pragma once
 
 namespace tr {
 
 struct CGTensorNode {
   Tensor<float> *data;
   Tensor<float> *gradient;
+  void *creator;
 };
 
-class CGMatsumNode {
-public:
-  void forward();
-
-  void set_rhs(CGTensorNode *rhs);
-  void set_lhs(CGTensorNode *lhs);
-  void set_res(CGTensorNode *res);
-
-private:
-  CGTensorNode *rhs;
+struct CGMatmulNode {
   CGTensorNode *lhs;
-
-  CGTensorNode *result;
-};
-
-class CGMatmulNode {
-public:
-  void forward();
-
-  void set_rhs(CGTensorNode *rhs);
-  void set_lhs(CGTensorNode *lhs);
-  void set_res(CGTensorNode *res);
-
-private:
   CGTensorNode *rhs;
-  CGTensorNode *lhs;
 
-  CGTensorNode *result;
+  CGTensorNode *out;
 };
-}; // namespace tr
+
+struct CGSumNode {
+  CGTensorNode *lhs;
+  CGTensorNode *rhs;
+
+  CGTensorNode *out;
+};
+
+} // namespace tr
--- src/layer.cpp
+++ src/layer.cpp
@@ -9,28 +9,27 @@ tr::Dense::Dense(size_t in_size, size_t out_size, bool bias) {
 
 tr::CGTensorNode *tr::Dense::compile(CGTensorNode *in) {
   tr::CGTensorNode *weights = new tr::CGTensorNode;
-  tr::CGMatmulNode *multiply_op = new tr::CGMatmulNode;
+  tr::CGMatmulNode *mul_op = new tr::CGMatmulNode;
 
-  tr::CGTensorNode *out = new tr::CGTensorNode;
+  mul_op->lhs = in;
+  mul_op->rhs = weights;
 
-  multiply_op->set_lhs(in);
-  multiply_op->set_rhs(weights);
+  tr::CGTensorNode *mul_out = new tr::CGTensorNode;
+  mul_out->creator = (void *)mul_op;
 
-  if (m_bias) {
-    tr::CGTensorNode *bias = new tr::CGTensorNode;
-    tr::CGTensorNode *multiply_out = new tr::CGTensorNode;
-    tr::CGMatsumNode *sum_op = new tr::CGMatsumNode;
-
-    multiply_op->set_res(multiply_out);
-
-    sum_op->set_rhs(multiply_out);
-    sum_op->set_lhs(bias);
-
-    sum_op->set_res(out);
-
-    return out;
-  } else {
-    multiply_op->set_res(out);
-    return out;
+  if (!m_bias) {
+    return mul_out;
   }
+
+  tr::CGTensorNode *bias = new tr::CGTensorNode;
+  tr::CGSumNode *sum_op = new tr::CGSumNode;
+  tr::CGTensorNode *sum_out = new tr::CGTensorNode;
+
+  sum_op->lhs = mul_out;
+  sum_op->rhs = bias;
+
+  sum_op->out = sum_out;
+  sum_out->creator = (void *)sum_out;
+
+  return sum_out;
 }
